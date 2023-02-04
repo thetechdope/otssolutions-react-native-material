710c0c5d8005d5190973fd1ca7643a4d8aab4e12
Bhautik Dobariya <bhautikdobariya98@gmail.com>
2023-02-04 17:13:18 +0530

    pan responder

--- src/20 Jan/screens/Profile/index.js
+++ src/20 Jan/screens/Profile/index.js
@@ -1,12 +1,66 @@
-import React from 'react';
-import { View, Text } from "react-native";
+import React, { useRef } from 'react';
+import { Animated, PanResponder, View } from "react-native";
 
 const Profile = () => {
-    return (
-        <View>
 
+    const pan = useRef(new Animated.ValueXY()).current;
+    const scale = useRef(new Animated.Value(1)).current;
+
+    const panResponder = React.useRef(
+        PanResponder.create({
+            // Ask to be the responder:
+            onStartShouldSetPanResponder: (evt, gestureState) => true,
+            onStartShouldSetPanResponderCapture: (evt, gestureState) =>
+                true,
+            onMoveShouldSetPanResponder: (evt, gestureState) => true,
+            onMoveShouldSetPanResponderCapture: (evt, gestureState) =>
+                true,
+
+            onPanResponderGrant: (evt, gestureState) => {
+                // The gesture has started. Show visual feedback so the user knows
+                // what is happening!
+                // gestureState.d{x,y} will be set to zero now
+            },
+            onPanResponderMove:  Animated.event([null, {dx: pan.x, dy: pan.y}]),
+            //
+            onPanResponderTerminationRequest: (evt, gestureState) =>
+                true,
+            onPanResponderRelease: (evt, gestureState) => {
+                Animated.spring(scale, {
+                    toValue : 0.5,
+                    friction : 5
+                }).start()
+                // The user has released all touches while this view is the
+                // responder. This typically means a gesture has succeeded
+            },
+            onPanResponderTerminate: (evt, gestureState) => {
+                // Another component has become the responder, so this gesture
+                // should be cancelled
+            },
+            onShouldBlockNativeResponder: (evt, gestureState) => {
+                // Returns whether this component should block native components from becoming the JS
+                // responder. Returns true by default. Is currently only supported on android.
+                return true;
+            },
+        }),
+    ).current;
+
+    return (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Animated.View
+                style={{
+                    height: 100,
+                    width: 100,
+                    backgroundColor: "red",
+                    transform: [{ translateX: pan.x }, { translateY: pan.y }, { scale : scale}],
+                }}
+                {...panResponder.panHandlers}
+            >
+
+            </Animated.View>
         </View>
     )
 }
+
 
 export default Profile
